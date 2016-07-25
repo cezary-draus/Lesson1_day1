27e3a5958c490ecf6dbd6d8f42c5a046dc52e49d
michalinab <michalina.bartosik@gmail.com>
2016-07-25 14:33:21 +0200

    What's your name

--- src/it/morfoza/Main.java
+++ src/it/morfoza/Main.java
@@ -1,11 +1,17 @@
 package it.morfoza;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        int result = 2 + 2;
-        System.out.println("Hello! How are you?");
-        System.out.println("Your resolt: " + result );
+        int result = 2 * 2;
+        System.out.println("Hello! What's your name?");
+
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+
+        System.out.println("Your name is: " + name);
 
     }
 }
