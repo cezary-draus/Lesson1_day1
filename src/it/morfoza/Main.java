c371120356f4a231142b3575bd736063ad4f395c
michalinab <michalina.bartosik@gmail.com>
2016-07-25 15:01:02 +0200

    Dodawanie

--- src/it/morfoza/Main.java
+++ src/it/morfoza/Main.java
@@ -1,17 +1,27 @@
 package it.morfoza;
 
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        int result = 2 * 2;
         System.out.println("Hello! What's your name?");
 
         Scanner input = new Scanner(System.in);
         String name = input.nextLine();
 
         System.out.println("Your name is: " + name);
+
+        System.out.println("Podaj zmienną a=");
+        int a = input.nextInt();
+        System.out.println("Podaj zmienną b=" );
+        int b = input.nextInt();
+
+        int c= a+b;
+        System.out.println(a + "+" + b + "=" + c);
+
 
     }
 }
