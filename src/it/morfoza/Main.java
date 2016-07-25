e3297543cc12c7012ce4897ab78ca40eacb27a76
michalinab <michalina.bartosik@gmail.com>
2016-07-25 11:44:13 +0200

    Hello and how are you - together

--- src/it/morfoza/Main.java
+++ src/it/morfoza/Main.java
@@ -3,7 +3,7 @@ package it.morfoza;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Witaj!");
-        System.out.println("Jak się masz?");
+        System.out.println("Witaj! Jak się masz?");
+
     }
 }
