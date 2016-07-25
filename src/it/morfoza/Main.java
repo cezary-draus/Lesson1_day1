04408436339d5215bed36d64930c2de99b6d1f56
michalinab <michalina.bartosik@gmail.com>
2016-07-25 15:51:38 +0200

    Wykonywanie dzialan za pomoca podanych symboli od uzytkownika

--- src/it/morfoza/Main.java
+++ src/it/morfoza/Main.java
@@ -7,21 +7,62 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello! What's your name?");
+        //System.out.println("Hello! What's your name?");
 
         Scanner input = new Scanner(System.in);
-        String name = input.nextLine();
+        //String name = input.nextLine();
 
-        System.out.println("Your name is: " + name);
+        //System.out.println("Your name is: " + name);
 
+        System.out.println("Podaj symbol działania:");
+        String symbol = input.nextLine();
         System.out.println("Podaj zmienną a=");
         int a = input.nextInt();
-        System.out.println("Podaj zmienną b=" );
+        System.out.println("Podaj zmienną b=");
         int b = input.nextInt();
 
-        int c= a+b;
-        System.out.println(a + "+" + b + "=" + c);
+        switch (symbol) {
+            case "+":
+                int c = a + b;
+                System.out.println(a + symbol + b + "=" + c);
+                break;
 
+            case "-":
+                int d = a - b;
+                System.out.println(a + symbol + b + "=" + d);
+                break;
 
+            case "*":
+                int e = a * b;
+                System.out.println(a + symbol + b + "=" + e);
+                break;
+
+            case "/":
+                int f = a / b;
+                System.out.println(a + symbol + b + "=" + f);
+                break;
+
+            default:
+                System.out.println("Ściemniasz");
+        }
+
+        /*if (symbol.equals("+")) {
+            int c = a + b;
+            System.out.println(a + symbol + b + "=" + c);
+        }
+
+        if (symbol.equals("-")){
+            int c = a - b;
+            System.out.println(a + symbol + b + "=" + c);
+        }
+        if (symbol.equals("*")) {
+            int c = a * b;
+            System.out.println(a + symbol + b + "=" + c);
+        }
+
+        if (symbol.equals("/")) {
+            int c = a / b;
+            System.out.println(a + symbol + b + "=" + c);
+        }*/
     }
 }
